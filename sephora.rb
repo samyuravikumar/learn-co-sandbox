a74cf9c8590012c1bf53dba3644074f00d93d9d8
samyuravikumar <samyuktha321@gmail.com>
2018-07-27 14:15:09 +0000

    Done.

--- sephora.rb
+++ sephora.rb
@@ -1,17 +1,20 @@
 products = {
+  
   "Naked Palette" => 54.00,
   
-  "Fenty Beauty Fairy Bomb Glittering Pom" => 42,
+  "Fenty Beauty Fairy Bomb Glittering Pom" => 42.00,
   
-  "Milk Makeup Kush High Volume Mascara" => 24,
+  "Milk Makeup Kush High Volume Mascara" => 24.00,
   
-  "Stila Stay All Day Liquid Lipstick" => 22,
+  "Stila Stay All Day Liquid Lipstick" => 22.00,
   
-  "Anastasia Brow Whiz" => 21,
+  "Anastasia Brow Whiz" => 21.00,
   
-  "Beauty Blender" => 20,
+  "Beauty Blender" => 20.00,
   
-  "NARS Radiant Creamy Concealer" => 30,
+  "NARS Radiant Creamy Concealer" => 30.00,
+  
+  "Tarte Shape Tape Concealer" => 27.00,
   
 }
 
@@ -19,46 +22,33 @@ cart = {}
 
 puts "Welcome to Sephora! What would you like to purchase?"
 puts products.keys
-
         
-def cartt(cart, products)
-  response = gets.chomp
+def add_to_cart(cart, products)
+  response = gets.strip
     puts "Good choice! That is $#{products[response]}. 
     
     Do you want to purchase it?"
-    response2 = gets.chomp
-    if response2 == "Yes"
+    response2 = gets.strip.downcase
+    if response2 == "yes"
       cart[response] = products[response]
-      # return cart
-      puts cart.values
       puts "Your cart contains: 
       #{cart.keys}"
     end
       puts "Are you interested in anything else?" 
-      response2 = gets.chomp
-      if response2 == "Yes"
+      response2 = gets.strip.downcase
+      if response2 == "yes"
         puts "Okay, let's keep going."
         puts "What else would you like? 
         
         Your options are:"
         puts products.keys
-          cartt(cart, products)
+        add_to_cart(cart, products)
         end
-     if response2 == "No"
+      if response2 == "no"
         puts "Thank you for shopping at Sephora! 
         
         Your total is: $#{cart.values.inject(:+)}"
-        
-        cartt(cart, products)
-      
-        if response3 =="Yes"
-          puts "Ok - Your options are #{products.keys}"
-        elsif response3 == "No"  
-          puts "Thank you for shopping at Sephora! 
-          Your total is: $#{cart.values.inject(:+)}"
-        else 
-          "Thank you for looking - have a great day!"
-      end
     end
 end
-cartt(cart, products)
+
+add_to_cart(cart, products)
